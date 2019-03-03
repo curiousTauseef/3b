589cdc860a662828213569de1b66285155ee794a
Ricardo Taboada <taboada.19@dartmouth.edu>
2019-03-03 14:37:02 -0500

    delete crap

--- ant.py
+++ ant.py
@@ -1,17 +1,6 @@
 import re
 import pymongo
 
-# server = "mongodb://Team08:3GfnR9a8WV4gR6lT@cluster0-shard-00-00-ppp7l.mongodb.net:27017,cluster0-shard-00-01-ppp7l.mongodb.net:27017,cluster0-shard-00-02-ppp7l.mongodb.net:27017/Team99DB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin"
-# db = None
-# try:
-#     client = pymongo.MongoClient(server)
-#     db = client.Team08DB
-#
-# except pymongo.errors.ServerSelectionTimeoutError as err:
-#     print('failed!')
-#     print(err)
-
-
 def insertPost(db, blogName, userName, title, postBody, tags, timestamp):
     collection = db.Blogs
     permalink  = blogName+'.'+ re.sub('[^0-9a-zA-Z]+', '_', title)
@@ -98,30 +87,3 @@ def delete(db, blogName, permalink, userName, timestamp):
     else:
         print("No post in DB with permalink: " + permalink)
 
-# def show(blogName)
-#insertPost("Time", "potatoMan", "TItleME", "Now this here is a body", [], "this is a time stamp")
-#insertComment(db, "userrrr", "2018-10-02T11:01:28.425Z ", "ThNNGNGNGNG GNG GNG ","swegggrgrt", "timestamp")
-
-#insertPost("Time", "potatoMan", "TItleME", "Now this here is a body", [], "this is a time stamp")
-#insertComment(db, "Time", "BBBBBB", "userrrr", "bfhfhfhfhfhfhodyy","BBBrgrgrgrgrgBBB")
-
-
-
-
-
-
-
-'''
-if db:
-    try:
-        post = {
-            "author": "Ben"
-        }
-        post_id = db.test.insert_one(post).inserted_id
-        print("post_id 1: {}".format(post_id))
-    except Exception as e:
-        print(e)
-else:
-    print('no db')
-
-'''
